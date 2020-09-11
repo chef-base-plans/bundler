77527b2d52517bfea0f616612c7b9f61a58d4bba
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-09-11 13:42:38 +0100

    Remove stderr from tests
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/bundler_works.rb
+++ controls/bundler_works.rb
@@ -16,7 +16,6 @@ control 'core-plans-bundler-works' do
   describe plan_installation_directory do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-    its('stderr') { should be_empty }
   end
   
   plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
@@ -29,7 +28,6 @@ control 'core-plans-bundler-works' do
       its('exit_status') { should eq 0 }
       its('stdout') { should_not be_empty }
       its('stdout') { should match /Bundler version #{plan_pkg_version}/  }
-      its('stderr') { should be_empty }
     end
   end
 end
