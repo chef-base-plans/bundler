2b3e2961a5ec0f3199b496c2b5b9ef2d323141b4
Sangameshwar Mandakanalli <smandaka@progress.com>
2021-12-10 11:59:18 +0530

    version bumped from 2.2.14 to 2.2.33
    Signed-off-by: Sangameshwar Mandakanalli <smandaka@progress.com>

--- plan.sh
+++ plan.sh
@@ -1,6 +1,6 @@
 pkg_name=bundler
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_version=2.2.14
+pkg_version=2.2.33
 pkg_origin=core
 pkg_license=('bundler')
 pkg_description="The Ruby language dependency manager"
